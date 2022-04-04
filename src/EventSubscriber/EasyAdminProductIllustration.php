2e4c1b370ab8fadf67850d5af175c0ffe92c88d0
marie-laure Debeaune <ml.debeaune@gmail.com>
2022-04-04 18:01:11 +0200

    update

--- src/EventSubscriber/EasyAdminProductIllustration.php
+++ src/EventSubscriber/EasyAdminProductIllustration.php
@@ -2,18 +2,63 @@
 
 namespace App\EventSubscriber;
 
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistEvent;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class EasyAdminSubscriber implements EventSubscriberInterface
 {
-    public static function getSubcribedEvents()
+    private $appKernel;
+
+    public function __construct(KernelInterface $appKernel)
+    {
+        $this->appKernel = $appKernel;
+    }
+
+    public static function getSubscribedEvents()
     {
         return [
             BeforeEntityPersistedEvent::class => ['setIllustration'],
+            BeforeEntityUpdatedEvent::class => ['updateIllustration']
         ];
     }
-    public function setIllustration(BeforeEntityPersistEvent $event)
+
+    public function uploadIllustration($event)
     {
         $entity = $event->getEntityInstance();
+
+        $tmp_name = $FILES['product']['tmp_name']['illustration'];
+        $filename = uniqid();
+
+        $extension = pathinfo($_FILES['product']['name']['illustration'],PATHINFO_EXTENSION); 
+
+        $project_dir= $this->appKernel->getProjectDir();
+
+        move_uploaded_file($tpm_name,$project_dir.'/public/uploads/'.$filename.'.'.$extension);
+
+        $entity->setIllustration($filename.'.'.$extension);
+    }
+
+    public function updateIllustration(BeforeEntityUpdatedEvent $event)
+    {
+        if (!($event->getEntityInstance() instanceof Product) || !($event->getEntityInstance() instanceof Header ))
+        {
+           return; 
+        }
+
+        if($_FILES['product']['tmp_name']['illustration'] != '')
+        {
+           $this->uploadIllustration($event);
+        }
+    }
+
+    public function setIllustration(BeforeEntityPersistEvent $event)
+    {
+        if (!($event->getEntityInstance() instanceof Product) || !($event->getEntityInstance() instanceof Header ))
+        {
+           return; 
+        }
+
+        $this->uploadIllustration($event);
     }
 }
