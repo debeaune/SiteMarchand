b994db409ba510b8e372fbee54ff4abe9e87489b
marie-laure Debeaune <ml.debeaune@gmail.com>
2022-04-08 17:46:14 +0200

    update

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -12,9 +12,15 @@ class ContactController extends AbstractController
     /**
      * @Route("/nous-contacter", name="contact")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->createForm(ContactType::class);
-        return render('contact/index.html.twig');
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted && $form->isValid())
+        {
+            $this->addFlash('notice','Merci de nous avour contacté. Notre équipe va vous répondre dans les meilleurs délais');
+        }    
+        return $this->render('contact/index.html.twig');   
     }
 }
