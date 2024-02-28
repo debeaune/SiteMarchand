0b8006d57dda153e896a6f91544d177fed1492b7
marie-laure Debeaune <ml.debeaune@gmail.com>
2024-02-28 19:09:26 +0100

    clean code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,28 +2,22 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
-use App\Entity\Header;
+
+use App\Repository\HeaderRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager = $entityManager;
-    }
 
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(ProductRepository $productRepository, HeaderRepository $headerRepository)
     {
-        $products = $this->entityManager->getRepository(Product::class)->findByIsBest(1);
-        $headers = $this->entityManager->getRepository(Header::class)->findAll();
+        $products = $productRepository->findByIsBest(1);
+        $headers = $headerRepository->findAll();
 
         return $this->render('home/index.html.twig',[
             'products' => $products,
